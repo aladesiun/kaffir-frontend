92db9d688c767a4d0f9b691ca53c5b6b2767d541
temitope <83991100+aladesiun@users.noreply.github.com>
2021-12-07 12:04:59 +1300

    ffed

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store/index'
 import notification from './components/layouts/notification'
 // import VueEcho from 'vue-echo-laravel'
 // import Pusher from 'pusher-js'
-import Echo from "laravel-echo"
+// import Echo from "laravel-echo"
 Vue.config.productionTip = false
 Vue.component('notification', notification);
 
@@ -17,15 +17,15 @@ Vue.use(Vuex);
 //     host: 'http://127.0.0.1:8000',
 // });
 
-window.Echo = new Echo(
-    {
-        broadcaster: 'pusher',
-        key: '0f018eca5868ce7a572a', 
-        cluster: 'eu',
-        encrypted: true,
-        // authEndpoint : 'http://localhost:8000/broadcasting/auth',
-    }
-); 
+// window.Echo = new Echo(
+//     {
+//         broadcaster: 'pusher',
+//         key: '0f018eca5868ce7a572a', 
+//         cluster: 'eu',
+//         encrypted: true,
+//         // authEndpoint : 'http://localhost:8000/broadcasting/auth',
+//     }
+// ); 
 
 new Vue({
     router: routes,
